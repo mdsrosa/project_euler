fd45f1bd4b2f436cc840dfa07a000db48b8e2f07
Matheus Rosa <matheusdsrosa@gmail.com>
2015-10-21 15:11:34 -0200

    Add solution for problem 1

---
+++ problem_1.py
@@ -0,0 +1,7 @@
+def sum_multiples_of(n1, n2, x):
+    def divisible_by(y, z): return y % z == 0
+    n = set()
+    for i in xrange(x):
+        if divisible_by(i, n1) or divisible_by(i, n2): n.add(i)
+    return sum(n)
+print sum_multiples_of(3, 5, 1000)
