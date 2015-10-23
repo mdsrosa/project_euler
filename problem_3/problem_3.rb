8dd78b3717c97931dfa2dc887d8c9481ff0a3181
Felipe Gentil <cdigentil@gmail.com>
2015-10-23 14:40:51 -0200

    Add solution for problem #3

---
+++ problem_3/problem_3.rb
@@ -0,0 +1,12 @@
+# Solves => https://github.com/mdsrosa/project_euler/issues/1
+
+def largest_prime_factor(number, highest_number)
+  (2..n).each do |i|
+    dividendo, mod = n.divmod(i)
+    if mod == 0
+      largest_prime_factor(dividendo, i)
+      break
+    end
+  end
+  puts highest_number if n == 1
+end
