cc540f409ed7267cfc14e70c790d083b49d11b28
Felipe Gentil <cdigentil@gmail.com>
2015-10-26 13:43:27 -0200

    Include ruby benchmark for problem #2

--- problem_2/solution.rb
+++ problem_2/solution.rb
@@ -1,12 +1,19 @@
-# Solves => https://github.com/mdsrosa/project_euler/issues/2
-s = 0
-a = 1
-b = 1
-c = a+b
-while c < 4000000
-  s += c
-  a = b+c
-  b = a+c
-  c = a+b
+def sum_even_fibonacci_numbers
+  sum = 0
+  first_item = 1
+  second_item = 1
+  third_item = first_item+second_item
+  while third_item < 4000000
+    sum += third_item
+    first_item = second_item + third_item
+    second_item = first_item + third_item
+    third_item = first_item + second_item
+  end
+  sum
 end
-puts s
+
+require 'benchmark'
+Benchmark.bm do |x|
+  x.report { sum_even_fibonacci_numbers }
+end
+puts "Answer: #{ sum_even_fibonacci_numbers }"
