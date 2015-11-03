2ee895c61f546f83f4b7fa0c6a2ba72578c378be
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-03 00:33:13 -0200

    Add a second Python implementation of problem 2

--- problem_2/solution.py
+++ problem_2/solution.py
@@ -1,6 +1,16 @@
-f1, f2, s, n = 0, 1, 0, 4000000
-while f2 < n:
-    f2, f1 = f1, f1 + f2
-    if f2 % 2 == 0:
-        s += f2
-print s
+def sum_even_fibonacci_numbers_1():
+    f1, f2, s, = 0, 1, 0,
+    while f2 < 4000000:
+        f2, f1 = f1, f1 + f2
+        if f2 % 2 == 0:
+            s += f2
+    return s
+def sum_even_fibonacci_numbers_2():
+    s, a, b = 0, 1, 1
+    c = a + b
+    while c < 4000000:
+        s += c
+        a = b + c
+        b = a + c
+        c = a + b
+    return s
