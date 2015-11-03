1e63d21d5751da12ad4104b6d2a0c170cc3898ff
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-03 00:55:00 -0200

    Add timing for problem 3's python implementation

--- problem_3/solution.py
+++ problem_3/solution.py
@@ -1,8 +1,12 @@
+import time
 def largest_prime_factor(n, h):
     for i in xrange(2, n+1):
         d, m = divmod(n, i)
         if m == 0:
             largest_prime_factor(d, i)
             break
-    if n == 1: print h
+    if n == 1: return h
+t1 = time.time()
 largest_prime_factor(600851475143, 0)
+t2 = time.time()
+print "=> largest_prime_factor(600851475143, 0): %fs" % (t2 - t1)
