980c1d7f1cbfc1d84e23fb3e845009721ab1bc26
Felipe Gentil <cdigentil@gmail.com>
2015-11-12 21:08:00 -0200

    Add Ruby solution for problem 5

---
+++ problem_5/solution.rb
@@ -0,0 +1,14 @@
+require 'prime'
+def smallest_positive_number_divisible_by_range(start_value=1, end_value=20)
+  primes = {}
+  (start_value..end_value).each do |value|
+    value.prime_division.each{ |value| primes[value[0]] = value[1] if primes[value[0]].to_i < value[1] }
+  end
+  primes.reduce(1){ |mem, value| mem *= value[0] ** value[1] }
+end
+
+require 'benchmark'
+Benchmark.bm do |x|
+  x.report { smallest_positive_number_divisible_by_range }
+end
+puts "Answer: #{smallest_positive_number_divisible_by_range}"
