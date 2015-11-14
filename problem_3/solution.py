476c73155c8f95d0858bed4057cde085d0f7c8ee
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-14 16:46:52 -0200

    Merge problem 3's Python solutions into one file

--- problem_3/solution.py
+++ problem_3/solution.py
@@ -1,12 +1,34 @@
 import time
-def largest_prime_factor(n, h):
+
+
+def largest_prime_factor_1(n, h):
     for i in xrange(2, n+1):
         d, m = divmod(n, i)
         if m == 0:
-            largest_prime_factor(d, i)
+            largest_prime_factor_1(d, i)
             break
-    if n == 1: return h
+    if n == 1:
+        return h
+
 t1 = time.time()
-largest_prime_factor(600851475143, 0)
+largest_prime_factor_1(600851475143, 0)
 t2 = time.time()
-print "=> largest_prime_factor(600851475143, 0): %fs" % (t2 - t1)
+
+print "=> largest_prime_factor_1(600851475143, 0): %fs" % (t2 - t1)
+
+
+def largest_prime_factor_2(n):
+    roots, product, x, y = [], 1, 2, n
+    while product != n:
+        while (y % x == 0):
+            roots.append(x)
+            y /= x
+            product *= roots[-1]
+        x += 1
+    return max(roots)
+
+t1 = time.time()
+largest_prime_factor_2(600851475143)
+t2 = time.time()
+
+print "=> largest_prime_factor_2(600851475143): %fs" % (t2 - t1)
