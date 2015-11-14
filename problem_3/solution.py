021b87e0d2d7a00ee15c045b4550fe5c944b3549
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-14 17:22:03 -0200

    Improved benchmark printing for python solutions of problem 3

--- problem_3/solution.py
+++ problem_3/solution.py
@@ -1,20 +1,28 @@
 import time
 
+result = None
+
 
 def largest_prime_factor_1(n, h):
+    global result
     for i in xrange(2, n+1):
         d, m = divmod(n, i)
         if m == 0:
             largest_prime_factor_1(d, i)
             break
     if n == 1:
-        return h
+        result = h
 
 t1 = time.time()
 largest_prime_factor_1(600851475143, 0)
 t2 = time.time()
 
+output_one = "=> largest_prime_factor_1(600851475143, 0): %s" % result
+
+print output_one
 print "=> largest_prime_factor_1(600851475143, 0): %fs" % (t2 - t1)
+
+print "=" * len(output_one)
 
 
 def largest_prime_factor_2(n):
@@ -28,7 +36,8 @@ def largest_prime_factor_2(n):
     return max(roots)
 
 t1 = time.time()
-largest_prime_factor_2(600851475143)
+output = largest_prime_factor_2(600851475143)
 t2 = time.time()
 
+print "=> largest_prime_factor_2(600851475143): %s" % output
 print "=> largest_prime_factor_2(600851475143): %fs" % (t2 - t1)
