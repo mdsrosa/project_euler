cf13ab010dd794fed4e027d3572ec278bce361a7
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-15 13:32:22 -0200

    Refactored results printing of python solution for problem 2

--- problem_2/solution.py
+++ problem_2/solution.py
@@ -1,4 +1,6 @@
 import time
+
+
 def sum_even_fibonacci_numbers_1():
     f1, f2, s, = 0, 1, 0,
     while f2 < 4000000:
@@ -6,6 +8,8 @@ def sum_even_fibonacci_numbers_1():
         if f2 % 2 == 0:
             s += f2
     return s
+
+
 def sum_even_fibonacci_numbers_2():
     s, a, b = 0, 1, 1
     c = a + b
@@ -16,10 +20,17 @@ def sum_even_fibonacci_numbers_2():
         c = a + b
     return s
 t1 = time.time()
-sum_even_fibonacci_numbers_1()
+output = sum_even_fibonacci_numbers_1()
 t2 = time.time()
-print "=> sum_even_fibonacci_numbers_1(): %fs" % (t2 - t1)
+
+print "=> Solution 1"
+print "=> Result: %s" % output
+print "=> Time: %fs" % (t2 - t1)
+
 t1 = time.time()
-sum_even_fibonacci_numbers_2()
+output = sum_even_fibonacci_numbers_2()
 t2 = time.time()
-print "=> sum_even_fibonacci_numbers_2(): %fs" % (t2 - t1)
+
+print "=> Solution 2"
+print "=> Result: %s" % output
+print "=> Time: %fs" % (t2 - t1)
