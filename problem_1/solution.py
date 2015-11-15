74b66d1241056a728429434663e0fa269b931ee0
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-15 13:30:17 -0200

    Add benchmark for Python solution of problem 1

--- problem_1/solution.py
+++ problem_1/solution.py
@@ -1,9 +1,18 @@
-# Solves => https://github.com/mdsrosa/project_euler/issues/1
+import time
+
 
 def sum_multiples_of(n1, n2, x):
-    def divisible_by(y, z): return y % z == 0
+    def divisible_by(y, z):
+        return y % z == 0
     n = set()
     for i in xrange(x):
-        if divisible_by(i, n1) or divisible_by(i, n2): n.add(i)
+        if divisible_by(i, n1) or divisible_by(i, n2):
+            n.add(i)
     return sum(n)
-print sum_multiples_of(3, 5, 1000)
+
+t1 = time.time()
+output = sum_multiples_of(3, 5, 1000)
+t2 = time.time()
+
+print "=> Result: %s" % output
+print "=> Time: %fs" % (t2 - t1)
