235e4e92582082b1032ed194185e92e2060d8208
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-15 13:33:12 -0200

    Refactored results printing of python solution for problem 4

--- problem_4/README.md
+++ problem_4/README.md
@@ -3,17 +3,20 @@ Largest palindrome product
 
 A palindromic number reads the same both ways.The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99. Find the largest palindrome made from the product of two 3-digit numbers.
 
+
+# Benchmarks
+
 ### Ruby
 ```ruby
 $ ruby problem_4/solution.rb
        user     system      total        real
    0.070000   0.000000   0.070000 (  0.075917)
-# => Answer: 906609
+Answer: 906609
 ```
 
 ### Python
 ```python
 $ python problem_4/solution.py
-largest_palindrome_from_the_product_of_three_digit_numbers: 0.074063s
-# => Answer: 906609
+=> Result: 906609
+=> Time: 0.081865s
 ```
--- problem_4/solution.py
+++ problem_4/solution.py
@@ -1,5 +1,9 @@
 import time
-def is_palindrome_number(n): return n == n[::-1]
+
+
+def is_palindrome_number(n):
+    return n == n[::-1]
+
 
 def largest_palindrome_from_the_product_of_three_digit_numbers():
     largest_number = 0
@@ -12,7 +16,9 @@ def largest_palindrome_from_the_product_of_three_digit_numbers():
     return largest_number
 
 t1 = time.time()
-largest_palindrome_from_the_product_of_three_digit_numbers()
+output = largest_palindrome_from_the_product_of_three_digit_numbers()
 t2 = time.time()
-print "largest_palindrome_from_the_product_of_three_digit_numbers: %fs" % (t2 - t1)
-print "Answer: {0}".format(largest_palindrome_from_the_product_of_three_digit_numbers())
+
+
+print "=> Result: %s" % output
+print "=> Time: %fs" % (t2 - t1)
