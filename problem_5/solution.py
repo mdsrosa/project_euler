92ba4bf5cd9ed0dc05c6de6c3947dd695087fcbd
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-15 13:33:45 -0200

    Refactored results printing of python solution for problem 5

--- problem_5/solution.py
+++ problem_5/solution.py
@@ -1,5 +1,15 @@
+import time
+
+
 def mdc(a, b):
-    while b: a, b = b, a % b
+    while b:
+        a, b = b, a % b
     return a
 mmc = lambda a, b: a * b / mdc(a, b)
-print reduce(mmc, xrange(2,20))
+
+t1 = time.time()
+output = reduce(mmc, xrange(2, 20))
+t2 = time.time()
+
+print "=> Result: %s" % output
+print "=> Time: %fs" % (t2 - t1)
