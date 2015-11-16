82863e948c30656b1413e57599275893c6a45629
Felipe Gentil <cdigentil@gmail.com>
2015-11-16 11:03:13 -0200

    Add Go solution for problem 6

---
+++ problem_6/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+  "math"
+  "time"
+)
+
+const MAX = 100
+const MIN = 1
+
+func sumOfTheSquares() int {
+  sum := float64((MAX + MIN) * (MAX - MIN + MIN) / 2)
+  return int(math.Pow(sum, 2))
+}
+
+func squareOfTheSums() int {
+  return (MAX * (MAX + MIN) * (2 * MAX + MIN))/6
+}
+
+func main() {
+  start := time.Now()
+  result := sumOfTheSquares() - squareOfTheSums()
+  elapsed := time.Since(start)
+  fmt.Printf("=> Result: %d\n", result)
+  fmt.Printf("=> Time: %.8f\n", elapsed.Seconds())
+}
