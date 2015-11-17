7dff23f78a2638dd18c78b00ece2bbde5f29b0d6
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-17 16:32:38 -0200

    Improves C solution of problem 4

--- problem_4/solution.c
+++ problem_4/solution.c
@@ -1,57 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <time.h>
 
 clock_t begin, end;
 double time_spent;
 
-void reverse_string(char *str){
-    // skip null
-    if (str == 0){ return; }
-    // skip empty string
-    if (*str == 0){ return; }
-    // get range
-    char *start = str;
-    char *end = start + strlen(str) - 1; // -1 for \0
-    char temp;
-    // reverse
-    while (end > start){
-    	temp = *start;
-    	*start = *end;
-    	*end = temp;
-    	++start;
-    	--end;
-    }
-}
+int is_palindrome(int n){
+	char n_str[10];
+	snprintf(n_str, sizeof(n_str), "%d", n); // convert int to str
+	
+	int length = strlen(n_str);
+	int start, end, middle;
+	
+	end = length - 1;
+	middle = length / 2;
+	
+	for(start = 0; start < middle; start++){
+		if(n_str[start] != n_str[end]){
+			break;
+		}
+		end--;
+	}
 
-int is_palindrome_number(int n){
-  char n_str[10], r_str[10];
-  snprintf(n_str, sizeof(n_str), "%d", n); // convert int to str
-  snprintf(r_str, sizeof(r_str), "%d", n);
-  reverse_string(0);
-  reverse_string(r_str);
-  if(strcmp(n_str, r_str) == 0){
-    return 0;
-  }else{
-    return -1;
-  }
+	if(start == middle)
+		return 0;
+		
+	return -1;
 }
 
 int main(int argc, char *argv[]){
-  begin = clock();
-
-  int x, y, v, largest_number;
-  for(x = 100; x < 999; x++){
-    for(y = 100; y < 999; y++){
-      v = x * y;
-      if(v > largest_number){
-        if(is_palindrome_number(v) == 0){
-          largest_number = v;
+	begin = clock();
+	
+	int x, y, v, largest_number;
+  
+    for(x = 100; x < 999; x++){
+      for(y = 100; y < 999; y++){
+        v = x * y;
+        if(v > largest_number && is_palindrome(v) == 0){
+            largest_number = v;
+          }
         }
       }
-    }
-  }
 
   end = clock();
   time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
