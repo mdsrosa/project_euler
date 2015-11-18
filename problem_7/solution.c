739f09227b15557b86653b6c4a2c3678171ddc70
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-18 21:04:11 -0200

    Fix indendation of C solution for problem 7

--- problem_7/solution.c
+++ problem_7/solution.c
@@ -8,64 +8,63 @@ clock_t begin, end;
 double time_spent;
 
 int is_prime(int n){
-	if(n == 1){
-		return 0;
-	}else{
-		if(n < 4){
-			return 1;
-		}else{
-			if(n % 2 == 0){
-				return 0;
-			}else{
-				if(n < 9){
-					return 1;
-				}else{
-					if(n % 3 == 0){
-						return 0;
-					}else{
-
-						int result, f;
-						result = floor(sqrt(n));
-						f = 5;
-						while(f <= result){
-							if(n % f == 0){
-								return 0;
-							}
-							if(n % (f + 2) == 0){
-								return 0;
-							}
-							f = f + 6;
-						}
-						return 1;
-					}
-				}
-			}
-		}
-	}
+    if(n == 1){
+        return 0;
+    }else{
+        if(n < 4){
+            return 1;
+        }else{
+            if(n % 2 == 0){
+                return 0;
+            }else{
+                if(n < 9){
+                    return 1;
+                }else{
+                    if(n % 3 == 0){
+                        return 0;
+                    }else{
+                        int result, f;
+                        result = floor(sqrt(n));
+                        f = 5;
+                        while(f <= result){
+                            if(n % f == 0){
+                                return 0;
+                            }
+                            if(n % (f + 2) == 0){
+                                return 0;
+                            }
+                            f = f + 6;
+                        }
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
 }
 
 int find_10001st_prime(){
-	int number, counter;
-	number = 1;
-	counter = 2;
-	while(counter <= LIMIT){
-		number = number + 2;
-		if(is_prime(number) == 1){
-			counter = counter + 1;
-		}
-	}
-	return number;
+    int number, counter;
+    number = 1;
+    counter = 2;
+    while(counter <= LIMIT){
+        number = number + 2;
+        if(is_prime(number) == 1){
+            counter = counter + 1;
+        }
+    }
+    return number;
 }
 
 int main(int argc, char *argv[]){
-	int result;
+    int result;
 
-	begin = clock();
-	result = find_10001st_prime();
-	end = clock();
+    begin = clock();
+    result = find_10001st_prime();
+    end = clock();
 
-	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-	printf("=> Result: %d\n", result);
-	printf("=> Time: %fs\n", time_spent);
+    printf("=> Result: %d\n", result);
+    printf("=> Time: %fs\n", time_spent);
 }
