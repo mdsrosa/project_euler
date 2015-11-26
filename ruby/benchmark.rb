9311044c2e4dddb29e0209559c301b47ae481b4f
Felipe Gentil <cdigentil@gmail.com>
2015-11-26 11:04:42 -0200

    Add a simple ruby benchmark class with correct output

---
+++ ruby/benchmark.rb
@@ -0,0 +1,14 @@
+module ProjectEuller
+  class RubyBenchmark
+
+    def self.run(&block)
+      before = Time.now
+      result = block.call
+      elapsed = '%.6fs' % (Time.now - before).to_s
+
+      puts "=> Result: #{result}"
+      puts  "=> Time: #{elapsed}"
+    end
+
+  end
+end
