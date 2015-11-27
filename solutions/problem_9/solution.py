fb76eecaff2a731ab392a09b8e7c0040a27ddc0e
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-27 16:04:12 -0200

    Add Python solution for problem 9

---
+++ solutions/problem_9/solution.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.dirname('./utils/python')))
+from python.decorators import timeit
+
+
+@timeit
+def solution():
+    n = 1000
+    for b in xrange(1, n):
+        for c in xrange(1, n):
+            a = n - b - c
+            if (a**2) + (b**2) == (c**2):
+                return a * b * c
+
+solution()
