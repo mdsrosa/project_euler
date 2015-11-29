8b115a3d6a3ecdd7fc6945bbce20680dc1cca119
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-29 00:58:00 -0200

    Add a second Python solution for problem 10

--- solutions/problem_10/solution.py
+++ solutions/problem_10/solution.py
@@ -3,13 +3,31 @@ import os
 sys.path.append(os.path.abspath(os.path.dirname('./utils/python')))
 from python.utils import timeit, is_prime
 
+LIMIT = 2000000
+
 @timeit
 def solution():
-    LIMIT = 2000000
     total_sum = 0
     for n in xrange(2, LIMIT):
         if is_prime(n):
             total_sum += n
     return total_sum
 
+@timeit
+def solution2():
+    total = 5
+    counter = 5
+    while counter <= LIMIT:
+        if is_prime(counter):
+            total += counter
+        counter += 2
+        if counter <= LIMIT and is_prime(counter):
+            total += counter
+        counter += 4
+
+    return total
+
+print "=> Solution 1"
 solution()
+print "=> Solution 2"
+solution2()
