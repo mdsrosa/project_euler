724fadaf637c6435f4b1e3cbf972a0b7bc584c2d
Carlos Eduardo Ribeiro <mail@carlosribeiro.me>
2015-11-30 02:55:05 -0300

    Add solution for clojure

---
+++ solutions/problem_1/solution.clj
@@ -0,0 +1,9 @@
+(defn divisibleBy3or5 [number]
+  (or
+    (zero? (mod number 3) )
+    (zero? (mod number 5) ) ))
+(def startTime (double (. java.lang.System (clojure.core/nanoTime))))
+(println (reduce + (filter divisibleBy3or5 (range 1000)) ) )
+(def endTime (double (. java.lang.System (clojure.core/nanoTime))))
+(def seconds (double (/ (- endTime startTime) 1000000000.0)))
+(println (str "Time: " seconds "s"))
