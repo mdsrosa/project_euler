af9c0e1cf0aae3ba2b289fcf8259bf4d103e6fe2
Matheus Rosa <matheusdsrosa@gmail.com>
2015-12-01 17:20:31 -0200

    Refactored C implementation for problem 2

--- solutions/problem_2/solution.c
+++ solutions/problem_2/solution.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-#include <time.h>
+#include "utils.h"
 
-clock_t begin, end;
-double time_spent;
+#define LIMIT 4000000L
 
-int main(int argc, char *argv[]){
-  begin = clock();
-
+int solution(){
   int s, a, b, c;
   s = 0;
   a = 1;
   b = 1;
   c = a + b;
-  while(c < 4000000L){
+
+  while(c < LIMIT){
     s += c;
     a = b + c;
     b = a + c;
     c = a + b;
   }
 
-  end = clock();
-  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  return s;
+}
 
-  printf("=> Result: %d\n", s);
-  printf("=> Time: %fs\n", time_spent);
+int main(int argc, char *argv[]){
+  return int_time_it(solution);
 }
