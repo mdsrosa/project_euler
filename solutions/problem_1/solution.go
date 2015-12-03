b41413fa6d5e059b766e1441fdbca4b10157bb8b
Felipe Gentil <cdigentil@gmail.com>
2015-12-03 13:42:37 -0200

    Add Go solution for problem 1

---
+++ solutions/problem_1/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func divisibleBy(num, divisor int) bool {
+  return num % divisor == 0
+}
+
+func SumMultipleOfThreeAndFive() (sum int) {
+  sum = 0
+  for i := 0; i < 1000; i++ {
+    if divisibleBy(i, 3) || divisibleBy(i, 5) {
+      sum += i
+    }
+  }
+  return
+}
+
+func main() {
+  start := time.Now()
+  result := SumMultipleOfThreeAndFive()
+  elapsed := time.Since(start)
+  fmt.Printf("=> Result: %d\n", result)
+  fmt.Printf("=> Time: %.8f\n", elapsed.Seconds())
+}
