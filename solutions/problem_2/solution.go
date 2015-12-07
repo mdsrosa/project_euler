1e937b11ed63f98156459b5dfa69a12b3ae8c9fe
Felipe Gentil <cdigentil@gmail.com>
2015-12-07 19:15:51 -0200

    Add another Go solution for problem 2

--- solutions/problem_2/solution.go
+++ solutions/problem_2/solution.go
@@ -28,10 +28,32 @@ func SumEvenFibonnaciNumbers() (sum int) {
   return
 }
 
+func SumEvenFibonnaciNumbers2() (sum int) {
+  sum = 0
+  a := 1
+  b := 1
+  c := a + b
+  for c < 4000000 {
+    sum += c
+    a = b + c
+    b = a + c
+    c = a + b
+  }
+  return
+}
+
 func main() {
   start := time.Now()
-  result := sumEvenFibonnaciNumbers()
+  result := SumEvenFibonnaciNumbers()
   elapsed := time.Since(start)
+  fmt.Println("=> Solution 1")
   fmt.Printf("=> Result: %d\n", result)
   fmt.Printf("=> Time: %.8f\n", elapsed.Seconds())
+
+  start2 := time.Now()
+  result2 := SumEvenFibonnaciNumbers2()
+  elapsed2 := time.Since(start2)
+  fmt.Println("=> Solution 2")
+  fmt.Printf("=> Result: %d\n", result2)
+  fmt.Printf("=> Time: %.8f\n", elapsed2.Seconds())
 }
