f3a4e97c620598dff06ab8f4f9ec5d902589fb40
Matheus Rosa <matheusdsrosa@gmail.com>
2015-12-09 17:51:20 -0200

    Add Perl solution for problem 1

---
+++ solutions/problem_1/solution.pl
@@ -0,0 +1,29 @@
+use Time::HiRes qw( time );
+
+sub divisible_by {
+	my ($y, $z) = @_;
+	$y % $z == 0
+}
+
+sub solution {
+	my ($n1, $n2, $limit) = @_;
+	my @numbers;
+
+	for $n (1..$limit-1) {
+		if(divisible_by($n, $n1) || divisible_by($n, $n2)){
+			push @numbers, $n;
+		}
+	}
+
+	my $total = 0;
+	$total += $_ for @numbers;
+
+	$total;
+}
+
+my $start = time();
+my $output = solution(3, 5, 1000);
+my $end = time();
+
+print "=> Result: ", $output, "\n";
+printf "=> Time: %fs\n", ($end - $start);
